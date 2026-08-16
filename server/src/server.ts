7ee0b31353ff1ce30133593a87c528fa7b1e452f
CodingCamp <thecodingcamp.contact@gmail.com>
2026-08-16 21:30:05 +0300

    Remove JWT token

--- server/src/server.ts
+++ server/src/server.ts
@@ -40,11 +40,6 @@ declare global {
   }
 }
 
-const jwtSecret = process.env.JWT_SECRET;
-
-if (!jwtSecret) {
-  throw new Error("JWT_SECRET is not defined");
-}
 
 const secretKey = new TextEncoder().encode(jwtSecret);
 const cookieName = "taskflow_token";
